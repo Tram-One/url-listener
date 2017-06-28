ca8ffab39df4ba103ebbefc25850b6fb89e32e83
jjurman <jjurman@constantcontact.com>
2017-06-28 04:28:10 -0400

    pushstate action happens AFTER push

--- listener.js
+++ listener.js
@@ -3,7 +3,8 @@ module.exports = (callback) => {
   window.addEventListener('popstate', callback, false)
   const pushState = window.history.pushState
   window.history.pushState = (state, ...args) => {
+    const returnValue = pushState.apply(history, [state].concat(args))
     callback({state})
-    return pushState.apply(history, [state].concat(args));
+    return returnValue;
   }
 }
